539991595d8465cd823bedfa14fac5311bf1f13d
Mustafa <hazim6163@gmail.com>
2019-09-30 05:45:43 +0200

    init project

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Redux Note',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Redux Note Example'
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'some body text'
+        ),
+      ),
+    );
+  }
+}
+
+
